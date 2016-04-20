267c9ddc537bc5a1c067e9a318f69c116f4c2453
Umur Kontaci <umur@umur.io>
2016-04-20 10:35:56 -0700

    Domains: Change the display of domain suggestion to Premium only

--- client/components/domains/domain-product-price/index.jsx
+++ client/components/domains/domain-product-price/index.jsx
@@ -4,7 +4,32 @@
 var React = require( 'react' ),
 	classNames = require( 'classnames' );
 
+/**
+ * Internal dependencies
+ */
+var Gridicon = require( 'components/gridicon' );
+
 var DomainProductPrice = React.createClass( {
+	subMessage() {
+		var freeWithPlan = this.props.cart && this.props.cart.hasLoadedFromServer && this.props.cart.next_domain_is_free && ! this.props.isFinalPrice;
+		if ( freeWithPlan ) {
+			return <span className="domain-product-price__free-text">{ this.translate( 'Free with your plan' ) }</span>;
+		} else if ( this.props.withPlansOnly && this.props.price ) {
+			return <small>{ this.translate( 'Included in the Premium Plan' ) } <Gridicon icon="lock" size={ 12 }/></small>;
+		}
+		return null;
+	},
+	priceText() {
+		if ( ! this.props.price ) {
+			return this.translate( 'Free' );
+		} else if ( this.props.withPlansOnly ) {
+			return null;
+		}
+		return this.translate( '%(cost)s {{small}}/year{{/small}}', {
+			args: { cost: this.props.price },
+			components: { small: <small /> }
+		} );
+	},
 	render: function() {
 		var freeWithPlan = this.props.cart && this.props.cart.hasLoadedFromServer && this.props.cart.next_domain_is_free && ! this.props.isFinalPrice,
 			classes = classNames( 'domain-product-price', { 'is-free-domain': freeWithPlan }, {
@@ -17,18 +42,8 @@ var DomainProductPrice = React.createClass( {
 
 		return (
 			<div className={ classes }>
-				<span className="domain-product-price__price">
-					{
-						this.props.price ?
-						this.translate( '%(cost)s {{small}}/year{{/small}}', {
-							args: { cost: this.props.price },
-							components: { small: <small /> }
-						} ) :
-						this.translate( 'Free' )
-					}
-				</span>
-
-				{ freeWithPlan ? <span className="domain-product-price__free-text">{ this.translate( 'Free with your plan' ) }</span> : null }
+				<span className="domain-product-price__price">{ this.priceText() }</span>
+				{ this.subMessage() }
 			</div>
 		);
 	}
